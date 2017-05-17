1c2e28f116ecd1bc2717ed3b4599a9ec47167abd
pkopachevskiy <pkopachevskiy@corp.finam.ru>
2017-05-17 13:53:43 +0300

    cp: error handling

--- cp/main.c
+++ cp/main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main(int argc, char *argv[]) {
     FILE *srcFile = stdin, *dstFile = stdout;
 
+    FILE *fileopen(char *, char *);
     void filecopy(FILE *, FILE *);
 
-    if (argc >= 2) {
-        srcFile = fopen(*++argv, "r");
+        if (argc >= 2) {
+        srcFile = fileopen(*++argv, "r");
     }
     if (argc >= 3) {
-        dstFile = fopen(*++argv, "w");
+        dstFile = fileopen(*++argv, "w");
     }
     filecopy(srcFile, dstFile);
+}
+
+FILE *fileopen(char *path, char *mode) {
+    FILE *fp;
+    if ((fp = fopen(path, mode)) == NULL) {
+        fprintf(stderr, "Failed to open file %s\n", path);
+        exit(1);
+    }
+    return fp;
+
 }
 
 void filecopy(FILE *src, FILE *dst) {
